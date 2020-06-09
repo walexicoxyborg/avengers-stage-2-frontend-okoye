d5e55ae7ed842a60722bf757133bc1ad9efdbdfa
EdoriAtiri <edoriatiri@gmail.com>
2020-06-09 17:22:15 +0100

    testimonials/facilities

--- app.js
+++ app.js
@@ -1,35 +1,3 @@
-const slider = document.querySelector('.slider');
-const leftArrow = document.querySelector('.left');
-const rightArrow = document.querySelector('.right');
-const indicatorParent = document.querySelector('.controls ul');
-
-let sectionIndex = 0;
-
-document.querySelectorAll('.controls li').forEach((indicator, index) => {
-  indicator.addEventListener('click', () => {
-    sectionIndex = index;
-    document.querySelector('.controls .selected').classList.remove('selected');
-    indicator.classList.add('selected');
-    slider.style.transform = 'translateX(' + sectionIndex * -25 + '%)';
-  });
-});
-
-leftArrow.addEventListener('click', () => {
-  sectionIndex = sectionIndex > 0 ? sectionIndex - 1 : 0;
-  document.querySelector('.controls .selected').classList.remove('selected');
-  indicatorParent.children[sectionIndex].classList.add('selected');
-  slider.style.transform = 'translateX(' + sectionIndex * -25 + '%)';
-});
-
-rightArrow.addEventListener('click', () => {
-  sectionIndex = sectionIndex < 3 ? sectionIndex + 1 : 3;
-  document.querySelector('.controls .selected').classList.remove('selected');
-  indicatorParent.children[sectionIndex].classList.add('selected');
-  slider.style.transform = 'translateX(' + sectionIndex * -25 + '%)';
-});
-
-// Facilities Testiomonial
-
 let index = 1;
 showdiv(index);
 
